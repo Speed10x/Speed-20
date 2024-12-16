044a314a4d74003ab28455a0e3b637534836c630
Hemant Kumar <160343394+Speed10x@users.noreply.github.com>
2024-12-16 13:08:24 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -79,9 +79,6 @@ async def start(client, message):
                 )
             ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        m=await message.reply_sticker("CAACAgQAAxkBAAIESGdckMdJQ4Ua86NE0Wm5WjqAwe9qAAJuDwAC4eqxUNoxB5joJxGiHgQ") 
-        await asyncio.sleep(1)
-        await m.delete()
         await message.reply_photo(
             photo=random.choice(PICS),
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
@@ -89,7 +86,6 @@ async def start(client, message):
             parse_mode=enums.ParseMode.HTML
         )
         return
-
     if AUTH_CHANNEL and not await is_subscribed(client, message):
         try:
             invite_link = await client.create_chat_invite_link(int(AUTH_CHANNEL), creates_join_request=True)
@@ -151,9 +147,6 @@ async def start(client, message):
             ]
         ]
         reply_markup = InlineKeyboardMarkup(buttons)
-        m=await message.reply_sticker("CAACAgQAAxkBAAIESGdckMdJQ4Ua86NE0Wm5WjqAwe9qAAJuDwAC4eqxUNoxB5joJxGiHgQ") 
-        await asyncio.sleep(1)
-        await m.delete()
         await message.reply_photo(
             photo=random.choice(PICS),
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
